01e30819831f8e0babbd6db7bf1da94e6de4a59b
larysa1387 <kaliuzhna.off@gmail.com>
2025-03-17 17:35:05 +0200

    remove unnecessary key prop from TodoInfo component

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,7 +4,6 @@ import { UserInfo } from '../UserInfo/UserInfo';
 export const TodoInfo = ({ todo }) => (
   <article
     className={`TodoInfo ${todo.completed === true ? 'TodoInfo--completed' : ''}`}
-    key={todo.id}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
