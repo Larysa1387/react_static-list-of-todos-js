f42e048774d68210f85ce6e6434f0ebce730f68b
larysa1387 <kaliuzhna.off@gmail.com>
2025-03-17 17:30:07 +0200

    add Todo components and styles; implement TodoList and TodoInfo

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,13 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import './TodoInfo.scss';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo }) => (
+  <article
+    className={`TodoInfo ${todo.completed === true ? 'TodoInfo--completed' : ''}`}
+    key={todo.id}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    {todo.user && <UserInfo user={todo.user} />}
+  </article>
+);
